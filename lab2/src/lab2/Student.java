f09eab375f35c1b1958157972e4e11c8d82ca64c
Student <Student@192.168.245.155>
2026-02-27 12:29:57 +0200

    git commit

---
+++ lab2/src/lab2/Student.java
@@ -0,0 +1,45 @@
+package lab2;
+
+public class Student {
+	///////// 
+	
+	private int id;
+	private String first_name;
+	private String last_name; // family name
+    //inca un comentariu
+	
+	Student(int id, String first_name, String last_name){
+		this.id = id;
+		this.first_name = first_name;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return id+"\t"+last_name +"\t" +first_name;
+	}
+	void print(){
+		int x = 100;
+	}
+	
+	void print2(){
+		int x = 100;
+	}
+	void print3(){
+		int y = 200;
+		System.out.println("de implementat print3 "+y);
+	}
+	
+	void print100000() {
+		//sdfsdfsd
+	}
+	
+	void print200000() {
+		System.out.println("sdfdsf");
+	}
+	
+	void print900000() {
+		System.out.println("sdfdsf");
+	}
+	
+}
---
+++ lab2/src/lab2/Teacher.java
@@ -0,0 +1,29 @@
+package lab2;
+
+
+/**
+Salut
+**/
+public class Teacher {
+	Teacher(){
+		System.out.println("Ctr. din Teacher");
+	}
+	
+	@Override
+	public String toString() {
+
+		return "from Teacher";
+	}
+	
+	
+	public String toClassroom() {
+
+		return "from Teacher";
+	}
+	
+		
+	public String toStudentPIP() {
+
+		return "from Teacher";
+	}
+}
